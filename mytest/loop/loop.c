5470ad6e0acf6fa6484232c3cf79ec9261175a95
Arijit Chattopadhyay <arijitnit06@gmail.com>
2013-10-08 15:39:39 -0400

    Adding the test for licm.

---
+++ mytest/loop/loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+	int i ;
+	int x = 0;
+	int y = 230;
+	int z;
+
+	for(i = 0 ; i < 100; i++) {
+		z= x+y;
+	}
+	return 0;
+}
--- mytest/pointeraa/result.s
+++ mytest/pointeraa/result.s
@@ -5,24 +5,20 @@ target triple = "x86_64-unknown-linux-gnu"
 define i32 @main() nounwind uwtable {
 entry:
   %retval = alloca i32, align 4
-  %a = alloca i32*, align 8
-  %b = alloca i32*, align 8
+  %a = alloca i32, align 4
+  %b = alloca i32, align 4
   %c = alloca i32, align 4
   %d = alloca i32, align 4
   store i32 0, i32* %retval
-  store i32* null, i32** %a, align 8
-  store i32* inttoptr (i64 20 to i32*), i32** %b, align 8
-  %0 = load i32** %a, align 8
-  %1 = load i32* %0, align 4
-  %2 = load i32* inttoptr (i64 20 to i32*), align 4
-  %add = add nsw i32 %1, %2
+  store i32 0, i32* %a, align 4
+  store i32 20, i32* %b, align 4
+  %0 = load i32* %a, align 4
+  %add = add nsw i32 %0, 20
   %add1 = add nsw i32 %add, 30
   store i32 %add1, i32* %c, align 4
-  %3 = load i32** %a, align 8
-  %4 = load i32* %3, align 4
-  %5 = load i32** %b, align 8
-  %6 = load i32* %5, align 4
-  %add2 = add nsw i32 %4, %6
+  %1 = load i32* %a, align 4
+  %2 = load i32* %b, align 4
+  %add2 = add nsw i32 %1, %2
   %add3 = add nsw i32 %add2, 40
   store i32 %add3, i32* %d, align 4
   ret i32 0
