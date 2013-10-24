5d876ac851de4fe74d11af9f679200caba20661e
Arijit Chattopahdyay <arijitnit06@gmail.com>
2013-10-24 00:01:55 -0400

    Adding the test files

---
+++ mytest/project/arijit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int i = 10;
+	if(i <100) {
+		i = i+10;
+	}else {
+		i = i-12;
+	}
+	if(i >4) {
+		i = i +30;
+	}else {
+		i = i+20;
+	}
+	return 0;
+}
---
+++ mytest/project/hook.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+void hook() {
+	cout<<"This is the Hook Function "<<endl;
+}
