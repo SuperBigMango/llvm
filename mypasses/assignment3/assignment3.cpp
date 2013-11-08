410a6375b30f819e64e9ba2962aa4f24de08f126
Arijit Chattopahdyay <arijitnit06@gmail.com>
2013-11-08 01:04:42 -0500

    Intermediate

--- mypasses/assignment3/assignment3.cpp
+++ mypasses/assignment3/assignment3.cpp
@@ -16,17 +16,6 @@ namespace {
 
 
 	class Ass3:public FunctionPass {
-		private:
-			vector<StringRef> instrFunctions;
-                        bool find(StringRef str) {
-				for(vector<StringRef>::iterator itr = instrFunctions.begin() ; itr != instrFunctions.end(); ++itr) {
-					if(*itr == str) {
-						return true;
-					}
-				}	
-				return false;
-			}
-
 		public:
 			static char ID;
 			Ass3();
@@ -34,8 +23,6 @@ namespace {
 	};
 
 	Ass3::Ass3():FunctionPass(ID) {
-		instrFunctions.push_back(StringRef("malloc"));
-		instrFunctions.push_back(StringRef("free"));
 	}
 
 	bool Ass3::runOnFunction(Function &func) {
@@ -45,52 +32,39 @@ namespace {
 				Instruction *inst = &*I;
 				CallInst *callInst = static_cast<CallInst*>(inst);
 				Function *targetFunction = callInst->getCalledFunction();
-			        if(find(targetFunction->getName())) {
-			        	errs() <<"Found the function"<<"\n";
-					errs()<<"Instruction is " <<*callInst<<"\n";
-					errs()<<"Tall : "<<*(callInst->getArgOperand(0))<<"\n";
-					if(targetFunction->getName() == "malloc") {
-						ConstantInt*size = dyn_cast<ConstantInt>(callInst->getArgOperand(0));
-						errs()<<"Size  is " <<*(size->getValue().getRawData())<<"\n";
-						
-						Constant* mallocHook = module->getOrInsertFunction("malloc_hook",
-								Type::getVoidTy(module->getContext()),
-								IntegerType::get(module->getContext(),64),
-								NULL) ;
+				if(targetFunction->getName() == "malloc") {
+					ConstantInt*size = dyn_cast<ConstantInt>(callInst->getArgOperand(0));
+					Constant* mallocHook = module->getOrInsertFunction("malloc_hook",
+							Type::getVoidTy(module->getContext()),
+							PointerType::get(IntegerType::get(module->getContext(), 8), 0),
+							IntegerType::get(module->getContext(),64),
+							NULL) ;
 
-						Function* mallocHookFunction = cast<Function>(mallocHook);
-						vector<Value*> argList;
-						argList.push_back(static_cast<Value*>(size));
-						Instruction *callInst = CallInst::Create(mallocHookFunction,argList);
-						callInst->insertAfter(inst);
-					}else if(targetFunction->getName() == "free") {
-						errs()<<"Number of operands "<<callInst->getNumOperands()<<"\n";
-						errs()<<"Sec : "<<*(callInst->getArgOperand(1))<<"\n";
-						Value *op = callInst->getArgOperand(0);
-						//LoadInst *ldInst = static_cast<LoadInst*>(callInst->getArgOperand(0));
-						//errs()<<"Load inst  " <<*(ldInst->getPointerOperand())<<"\n";
-				                // op  = ldInst->getPointerOperand();
-						//Constant* freeHook = module->getOrInsertFunction("free_hook",
-						//		Type::getVoidTy(module->getContext()),
-						//		IntegerType::get(module->getContext(),64),
-						//		NULL) ;
-					        Constant* freeHook = module->getOrInsertFunction("free_hook",
-					        		Type::getVoidTy(module->getContext()),
-					        		PointerType::get(IntegerType::get(module->getContext(), 8), 0),
-					        		NULL);		
+					Function* mallocHookFunction = cast<Function>(mallocHook);
+					vector<Value*> argList;
+					argList.push_back(CastInst::CreatePointerCast(callInst,
+								PointerType::get(IntegerType::get(module->getContext(), 8), 0),
+								"mallocHookCast",callInst));
+					argList.push_back(static_cast<Value*>(size));
+					Instruction *callInst = CallInst::Create(mallocHookFunction,argList);
+					callInst->insertAfter(inst);
+				}else if(targetFunction->getName() == "free") {
+					Value *op = callInst->getArgOperand(0);
+					Constant* freeHook = module->getOrInsertFunction("free_hook",
+							Type::getVoidTy(module->getContext()),
+							PointerType::get(IntegerType::get(module->getContext(), 8), 0),
+							NULL);		
 
-					        Function* freeHookFunction = cast<Function>(freeHook);
-					        vector<Value*> freeArgList;
-						Type *pTy = PointerType::get(IntegerType::get(module->getContext(),8),0);
-						freeArgList.push_back(ConstantExpr::getIntToPtr(dyn_cast<Constant>(op), pTy));
-					       // freeArgList.push_back(CastInst::CreatePointerCast(op,
-					        //		PointerType::get(IntegerType::get(module->getContext(), 8), 0),"Ari",inst));
-					        //Instruction *freeCallInst = CallInst::Create(freeHookFunction,freeArgList);
-					        //callInst->insertAfter(freeCallInst);
-					}
-			        }
-				errs()<<"\n\n\n";
+					Function* freeHookFunction = cast<Function>(freeHook);
+					vector<Value*> freeArgList;
+					freeArgList.push_back(CastInst::CreatePointerCast(op,
+								PointerType::get(IntegerType::get(module->getContext(), 8), 0),
+								"freeHookCast",inst));
+					Instruction *freeCallInst = CallInst::Create(freeHookFunction,freeArgList);
+					freeCallInst->insertBefore(inst);
+				}
 			}
+			errs()<<"\n\n\n";
 		}
 		return true;
 	}
